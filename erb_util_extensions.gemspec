3eef3a509836bc2adc3615f3d333e036469a2060
WebGet <webget@webget.com>
2009-03-18 10:32:57 -0700

    first

---
+++ erb_util_extensions.gemspec
@@ -0,0 +1,20 @@
+Gem::Specification.new do |s|
+
+  s.name              = "webget_ruby_erb_util_extensions"
+  s.summary           = "WebGet.com Ruby ERB Util extensions for outputting HTML tags"
+  s.version           = "1.0.4"
+  s.author            = "WebGet"
+  s.email             = "webget@webget.com"
+  s.homepage          = "http://webget.com/"
+  s.signing_key       = '/home/webget/keys/certs/webget.com.rsa.private.key.and.certificate.pem'
+  s.cert_chain        = ['/home/webget/keys/certs/webget.pem']
+
+  s.platform          = Gem::Platform::RUBY
+  s.require_path      = 'lib'
+  s.has_rdoc          = true
+  s.files             = ['lib/erb_util_extensions.rb']
+
+  s.add_dependency('webget_ruby_html', '>=1.0.4')
+  s.add_dependency('actionpack', '>=2.2.2')
+
+end
---
+++ lib/erb_util_extensions.rb
@@ -0,0 +1,36 @@
+# = Ruby Gem: ERB Util Extensions
+#
+# Author:: Joel Parker Henderson, joelparkerhenderson@gmail.com
+# Copyright:: Copyright (c) 2006-2009 Joel Parker Henderson
+# License:: CreativeCommons License, Non-commercial Share Alike
+# License:: LGPL, GNU Lesser General Public License
+#
+# Wrap the methods from the html_output gem into module ERB::Util
+##
+
+require 'html_output'
+require 'actionpack'
+require 'erb' 
+
+class ERB
+ module Util
+  include HTMLOutput
+  module_function :table
+  module_function :tables
+  module_function :thead
+  module_function :tbody
+  module_function :tfoot
+  module_function :th
+  module_function :ths
+  module_function :tr
+  module_function :trs
+  module_function :td
+  module_function :tds
+  module_function :ul
+  module_function :uls
+  module_function :li
+  module_function :lis
+  module_function :attrs_to_string
+ end
+
+end
